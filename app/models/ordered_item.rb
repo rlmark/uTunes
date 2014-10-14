dfeb98683052e3bea0e7cce976cbb881e173fd5c
Rebecca Mark <rebecca.lmark@gmail.com>
2014-10-14 16:05:28 -0700

    Create relationships between models

--- app/models/ordered_item.rb
+++ app/models/ordered_item.rb
@@ -1,2 +1,5 @@
 class OrderedItem < ActiveRecord::Base
+  belongs_to :cart
+  belongs_to :product
+  belongs_to :merchant, through: :product
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ActiveRecord::Base
+  belongs_to :merchant
+  has_many :ordered_items
+  has_many :ratings
+  has_and_belongs_to_many :categories
 end
