d62525467eea5db0df0d9bf1a54452d2182ceb45
Rebecca Mark <rebecca.lmark@gmail.com>
2014-10-14 15:15:15 -0700

    Products and Ratings controllers created

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,29 @@
+class ProductsController < ApplicationController::Base
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def check_stock
+
+  end
+
+  def move_to_cart
+
+  end
+
+  # Reture might be useful for temporary retirement
+  def retire
+
+  end
+
+  # Delete is for no longer selling the product
+  def delete
+
+  end
+
+end
---
+++ app/controllers/ratings_controller.rb
@@ -0,0 +1,10 @@
+class RatingsController < ApplicationController
+  def new
+
+  end
+
+  def create
+
+  end
+
+end
