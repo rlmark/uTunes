aa9bacf65197cb9ba1b3c4c3c434ed51dacaec91
Rebecca Mark <rebecca.lmark@gmail.com>
2014-10-15 10:29:44 -0700

    Changed name in product table from name to albumname

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141015170936) do
+ActiveRecord::Schema.define(version: 20141015172158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,7 +62,7 @@ ActiveRecord::Schema.define(version: 20141015170936) do
   end
 
   create_table "products", force: true do |t|
-    t.string   "name"
+    t.string   "album_name"
     t.text     "description"
     t.integer  "merchant_id"
     t.integer  "stock"
