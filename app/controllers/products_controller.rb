aa93f628e70f48a2d04a1696bf7429ae097b5f3e
Crystal Perreira <cvperreira@gmail.com>
2014-10-16 15:44:28 -0700

    edit products from dashboard

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,6 +11,19 @@ class ProductsController < ApplicationController
   def create
 
   end
+  
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(params.require(:product).permit(:artist, :album_name, :image, :stock, :price))
+    redirect_to dashboard_path
+    else
+    render :edit_product
+    end
+  end
 
   def check_stock
 
