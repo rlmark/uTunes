b88d36f6f4fcaf2e01b66518199e682e3f726af3
Rebecca Mark <rebecca.lmark@gmail.com>
2014-10-21 14:28:17 -0700

    working validation for ratings

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,3 +1,6 @@
 class Rating < ActiveRecord::Base
+  validates :stars, presence: true
+  validates :stars, inclusion: {:in =>1..5, :message => "You an only give a rating between 1 and 5 stars"} 
+
   belongs_to :product
 end
