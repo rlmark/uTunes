40c901d070f541aae2920733566a7df478ece571
Crystal Perreira <cvperreira@gmail.com>
2014-10-22 12:09:46 -0700

    not much

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,16 +4,17 @@ class CategoriesController < ApplicationController
   end
 
   def new
-    @categories = Category.all
     @category = Category.new
+    @categories = Category.all
   end
 
   def create
     @category = Category.new(params.require(:category).permit(:name))
     if @category.save
       redirect_to new_categories_path
-    else 
-      render new_categories_path
+    else
+      @categories = Category.all
+      render :new
     end
   end
 
