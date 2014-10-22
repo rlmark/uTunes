13b3232cbffdb9e1c3996e0051e5c31f98e41322
Brenda Praggastis <bpraggastis@gmail.com>
2014-10-21 21:51:03 -0700

    Customer Information is entered and saved/ confirmation page printed

--- app/controllers/ordered_items_controller.rb
+++ app/controllers/ordered_items_controller.rb
@@ -12,8 +12,15 @@ class OrderedItemsController < ApplicationController
   def update_quantity
     quantity = params[:ordered_item][:quantity].to_i
     record = OrderedItem.find(params[:ordered_item][:item_id])
-    if record != nil && quantity >= 0
-      record.quantity = quantity
+    if record != nil && quantity > 0
+      in_stock = Product.find(record.product_id).stock
+
+      record.quantity = [quantity,in_stock].min
+      record.save
+    end
+
+    if record != nil && quantity == 0
+      record.status = "cancelled"
       record.save
     end
     redirect_to show_cart_path
