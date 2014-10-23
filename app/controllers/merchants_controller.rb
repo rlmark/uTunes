02c01f5d637110ef53631f98864c87745ae29c46
Rebecca Mark <rebecca.lmark@gmail.com>
2014-10-23 12:09:36 -0700

    cleand up unused comments

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -61,24 +61,9 @@ class MerchantsController < ApplicationController
   end
 
   def change_status
-    # gets cart object using params id
     cart = Cart.find(params[:id])
-    #
-    # # queues up all ordered items and then returns their associated product object.
-    # all_products = cart.ordered_items.collect{|i| i.product }
-    #
-    # # returns all merchant's product instances
-    # # need to ad && i.ordered_items.cart id == params[:id] because this
-    # merchant_products = all_products.select{|i| i.merchant_id == session[:merchant_id]}
-    #
-    # # nested loop takes all merchants product instances, gets their ordered items(an array), then iterates throught hat array chanign the status of the individual ordered item to shipped.
-    # merchant_products.each{|i| i.ordered_items.each{|ord_i| ord_i.status = "shipped"; ord_i.save}}
-
     orders = cart.ordered_items.includes(:product).where(products: {merchant_id: session[:merchant_id]})
-
-    # orders.update_all(status: "shipped")
     orders.each{|order| order.status = "shipped"; order.save}
-
     redirect_to orders_path
   end
 
