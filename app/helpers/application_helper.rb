ae1aad2f4cb191f0eb1eaebea0fb93406880f84d
Rebecca Mark <rebecca.lmark@gmail.com>
2014-10-24 13:28:25 -0700

    debugged cart incrementing funciton

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,12 @@
 module ApplicationHelper
   # create a sessions helper.
   def show_cart
-    cart = Cart.find(session[:cart_id])
-    cart.ordered_items.length
+    if session[:cart_id] != nil
+      cart = Cart.find(session[:cart_id])
+      cart.ordered_items.length
+    else
+      "0"
+    end
   end
 
 end
